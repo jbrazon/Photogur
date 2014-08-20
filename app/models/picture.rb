67d1f0cafe8d83d964ed18215583416e30e88f28
Jon Lyons <jrl08d@my.fsu.edu>
2014-08-20 18:10:28 -0400

    resized pictures

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,5 @@
 class Picture < ActiveRecord::Base
-	 has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	 has_attached_file :avatar, :styles => { :thumb => "100x100#", :medium => "500x500" }, :default_url => "/images/:style/missing.png"
  	 validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
  	 has_many :comments
 
