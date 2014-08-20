a0b6a4c58d21f186676d591d0db12679fc1d5d68
Jon Lyons <jrl08d@my.fsu.edu>
2014-08-20 13:02:09 -0400

    first git commit for photo app

---
+++ app/models/picture.rb
@@ -0,0 +1,17 @@
+class Picture < ActiveRecord::Base
+	 has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+ 	 validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+	def picture_show(size)
+  	# no url but there is an avatar
+	  	if self.url.nil? && !(self.avatar_content_type.nil?)
+	  		self.avatar.url(size)
+	  	# no avatar but there is a url
+	  	elsif !(self.url.nil?) && self.avatar_content_type.nil?
+	  		self.url
+	  	# otherwise
+		else
+			"nothing to see here"
+		end
+	end
+end
